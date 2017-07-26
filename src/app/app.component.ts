06bad4ab72adf538f1ad8acb23b6be09c3dae114
Naim Jeem <naim36208@gmail.com>
2017-07-26 16:19:28 +0600

    simple carousel app has some bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  items: Array<any> = [];
+
+  constructor() {
+    this.items = [
+      { name: '/assets/img/c1.png' },
+      { name: '/assets/img/c2.png' },
+      { name: '/assets/img/c3.png' },
+      { name: '/assets/img/c4.png' },
+      { name: '/assets/img/c10.png' },
+      { name: '/assets/img/c6.png' }
+    ];
+  }
 }
